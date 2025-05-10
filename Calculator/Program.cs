645ae7cb317b39ccd64adffa31b9b86ffa746938
Abdulrahaman27 <abdulrahamanabbas27@gmail.com>
2025-05-10 05:38:14 +0100

    Updated calculator error response and better console interaction.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Calculator
 {
@@ -10,53 +7,70 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            int num1;
-            int num2;
-            string operation;
-            Console.WriteLine("Hello, Welcome to the calculator program!");
-            Console.WriteLine("Please enter your first number: ");
-            //num1 = Convert.ToInt32(Console.ReadLine());
-            while (!int.TryParse(Console.ReadLine(), out num1))
+            Console.OutputEncoding = Encoding.UTF8;
+            bool calculatorProgram = true;
+            while (calculatorProgram)
             {
-                Console.WriteLine("Please enter a valid number!");
-            }
+                int num1;
+                int num2;
+                string operation;
+                Console.WriteLine("Hello👋, Welcome to the calculator program!");
 
-            Console.WriteLine("Please enter your second number: ");
-            while (!int.TryParse(Console.ReadLine(), out num2))
-            {
-                Console.WriteLine("Please enter a valid number!");
-            }
 
-            Console.WriteLine("What sort of operation would you like to do: ");
-            Console.WriteLine("\n a for addition\n s for subtraction\n m for multiplication\n d for division");
-            operation = Console.ReadLine().ToLower();
-            while (string.IsNullOrEmpty(operation) || (operation != "a" && operation != "s" && operation != "m" && operation != "d"))
-            {
-                Console.WriteLine("\nPlease enter a valid operation");
+                Console.WriteLine("Please enter your first number: ");
+                while (!int.TryParse(Console.ReadLine(), out num1))
+                {
+                    Console.WriteLine("❌ : Please enter a valid number!");
+                }
+
+                Console.WriteLine("Please enter your second number: ");
+                while (!int.TryParse(Console.ReadLine(), out num2))
+                {
+                    Console.WriteLine("❌ : Please enter a valid number!");
+                }
+
+                Console.WriteLine("What sort of operation would you like to do: ");
                 Console.WriteLine("\n a for addition\n s for subtraction\n m for multiplication\n d for division");
-                operation = Console.ReadLine();
-            }
-
-            switch (operation)
-            {
-                case "a":
-                    Console.WriteLine("The addition of " + num1 + " and " + num2 + " = " + (num1 + num2));
-                    break;
-                case "s":
-                    Console.WriteLine("The subtraction of " + num1 + " from " + num2 + " = " + (num1 - num2));
-                    break;
-                case "m":
-                    Console.WriteLine("The multiplication of " + num1 + " and " + num2 + " = " + (num1 * num2));
-                    break;
-                case "d":
-                    Console.WriteLine("The division of " + num1 + " by " + num2 + " = " + (num1 / num2));
-                    break;
-                default:
+                operation = Console.ReadLine().ToLower();
+                while (string.IsNullOrEmpty(operation) || (operation != "a" && operation != "s" && operation != "m" && operation != "d"))
+                {
+                    Console.WriteLine("\n❌ : Please enter a valid operation");
                     Console.WriteLine("\n a for addition\n s for subtraction\n m for multiplication\n d for division");
-                    break;
+                    operation = Console.ReadLine();
+                }
+
+                switch (operation)
+                {
+                    case "a":
+                        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                        break;
+                    case "s":
+                        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                        break;
+                    case "m":
+                        Console.WriteLine($"{num1} * {num2} = {num1 * num2}");
+                        break;
+                    case "d":
+                        if(num2  == 0)
+                        {
+                            Console.WriteLine("❌ : Can't divide by 0");
+                        }
+                        else { 
+                            Console.WriteLine($"{num1} / {num2} = {(double)num1 / num2}");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("\n a for addition\n s for subtraction\n m for multiplication\n d for division");
+                        break;
+                }
+            Console.WriteLine("Would you like to carry out another Calculation?(y/n)");
+            string answer = Console.ReadLine().ToLower();
+                if (answer != "yes" && answer != "y")
+                {
+                    calculatorProgram = false;
+                }
             }
-
-
+            Console.WriteLine("Thanks for using the calculator!💯");
             Console.ReadKey();
         }
     }
